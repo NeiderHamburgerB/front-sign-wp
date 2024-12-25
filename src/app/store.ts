1519365ae0bf9927863532097e86a561c12010f2
Neider Hamburger <87545790+NeiderHamburgerB@users.noreply.github.com>
2024-12-25 18:39:30 -0500

    fix: configure persistor

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,13 +1,27 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; 
 import checkoutReducer from '../features/checkout/reducers/checkoutSlice';
 import productsReducer from '../features/products/reducers/productsSlice';
+import { combineReducers } from 'redux';
+
+const persistConfig = {
+  key: 'root', 
+  storage,
+};
+
+const rootReducer = combineReducers({
+  products: productsReducer,
+  checkout: checkoutReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    products: productsReducer,
-    checkout: checkoutReducer,
-  },
+  reducer: persistedReducer,
 });
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { Provider } from 'react-redux';
-import { store } from './app/store';
+import { persistor, store } from './app/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-     <Provider store={store}>
-      <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </StrictMode>,
-)
+);
